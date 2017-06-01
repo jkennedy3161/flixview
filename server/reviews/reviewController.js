0455968a93e4c81b5d4d7bf2afab33442561bfa8
jkennedy3161 <kennedyjt88@gmail.com>
2017-06-01 11:14:11 -0700

    (feat) build out post, delete method for review model's controller

--- server/reviews/reviewController.js
+++ server/reviews/reviewController.js
@@ -2,6 +2,23 @@ var Review = require('./reviewModel.js');
 
 module.exports = {
   postReview: function(req, res, next) {
+    var data = req.body;
+    var review = new Review({
+      username: data.username,
+      movie: data.movie,
+      title: data.title,
+      date: new Date(),
+      review: data.review,
+      rating: data.rating,
+      voteCount: 0
+    });
+    review.save(function(err, review) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.sendStatus(201).send('Review saved!');
+      }
+    })
 
   },
   getReview: function(req, res, next) {
@@ -18,6 +35,16 @@ module.exports = {
 
   },
   deleteReview: function(req, res, next) {
+    var id = req.params.reviewId;
+    Review.find({id: id})
+      .remove()
+      .exec(function(err, data) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.sendStatus(200).send('Review successfully removed!');
+        }
+      });
 
   },
   editReview: function(req, res, next) {
