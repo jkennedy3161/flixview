baf881a582863633fa6d5625f72423f85a8ae0ea
jkennedy3161 <kennedyjt88@gmail.com>
2017-06-01 10:34:24 -0700

    (feat) build CRUD methods for reviewController routes

---
+++ server/reviews/reviewController.js
@@ -0,0 +1,19 @@
+var Review = require('./reviewModel.js');
+
+module.exports = {
+  postReview: function(req, res, next) {
+
+  },
+  getReview: function(req, res, next) {
+
+  },
+  deleteReview: function(req, res, next) {
+
+  },
+  editReview: function(req, res, next) {
+
+  },
+  editCount: function(req, res, next) {
+
+  }
+};
