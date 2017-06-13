8abba4123eae57919f8b26583b0495ecf10c64dc
jkennedy3161 <kennedyjt88@gmail.com>
2017-06-13 10:18:37 -0700

    (fix) mongo crashes

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var path = require('path');
 
 var port = process.env.PORT || 8000;
 
-var db = process.env.MONGOLAB_URI || 'mongodb://localhost/flixview';
+var db = process.env.MONGOLAB_URI || 'mongodb://localhost:' + port + '/flixview';
 
 app.use(express.static('client'));
 app.use(bodyParser.json());
