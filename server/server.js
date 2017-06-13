f2d8dd15ea8f9d2aad6d7dbb9107e68ac9a4a850
jkennedy3161 <kennedyjt88@gmail.com>
2017-06-13 10:53:31 -0700

    (fix)correct how to connect to mongodb

--- server/server.js
+++ server/server.js
@@ -6,13 +6,18 @@ var path = require('path');
 
 var port = process.env.PORT || 8000;
 
-var db = process.env.MONGOLAB_URI || 'mongodb://localhost:' + port + '/flixview';
+var db = process.env.MONGODB_URI || 'mongodb://localhost:' + port + '/flixview';
 
 app.use(express.static('client'));
 app.use(bodyParser.json());
 
 mongoose.Promise = global.Promise;
-mongoose.connect(db);
+mongoose.connect(db, function(err, res) {
+  if (err) {
+    return 'Could not connect due to ' + err + '';
+  }
+  return res;
+});
 
 require('./config/routes')(app, express);
 
